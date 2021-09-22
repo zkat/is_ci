301f0a9ba418da349140e93f2c031f2fd83c9a8a
Kat Marchán <kzm@zkat.tech>
2021-09-22 09:32:49 -0700

    feat(api): Add initial API

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Parking this name real quick
+This crate tells you if you're in a CI environment or not. It does not tell
+you **which** you're in, but it makes a good effort to make sure to accurately
+tell you whether you're in one.
+
+This crate is based on the
+[`@npmcli/ci-detect`](https://npm.im/@npmcli/ci-detect) package.
+
+If you need more information about the specific CI environment you're running
+in and you can handle a heavier dependency, please consider using
+[`ci_info`](https://crates.io/crates/ci_info) instead.
+
+## Example
+
+```rust
+// This is it. This is the entire API.
+let am_i_in_ci_right_now = is_ci::is_ci();
+```
+
+## License
+
+`is_ci` is released to the Rust community under the [ISC License](./LICENSE).
+
+It is based on `@npmcli/ci-detect` which is released to the community under
+the [ISC License](https://github.com/npm/ci-detect/blob/main/LICENSE).
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,47 @@
+/// Returns true if the current environment is found to probably be a CI
+/// environment or service. That's it, that's all it does.
+pub fn is_ci() -> bool {
+    let ci_var = std::env::var("CI");
+    ci_var == Ok("true".into())
+        || ci_var == Ok("1".into())
+        || check("CI_NAME")
+        || check("GITHUB_ACTION")
+        || check("GITLAB_CI")
+        || check("NETLIFY")
+        || check("TRAVIS")
+        || matches!(std::env::var("NODE"), Ok(node) if node.ends_with("//heroku/node/bin/node"))
+        || check("CODEBUILD_SRC_DIR")
+        || check("BUILDER_OUTPUT")
+        || check("GITLAB_DEPLOYMENT")
+        || check("NOW_GITHUB_DEPLOYMENT")
+        || check("NOW_BUILDER")
+        || check("BITBUCKET_DEPLOYMENY")
+        || check("GERRIT_PROJECT")
+        || check("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI")
+        || check("BITRISE_IO")
+        || check("BUDDY_WORKSPACE_ID")
+        || check("BUILDKITE")
+        || check("CIRRUS_CI")
+        || check("APPVEYOR")
+        || check("CIRCLECI")
+        || check("SEMAPHORE")
+        || check("DRONE")
+        || check("DSARI")
+        || check("TDDIUM")
+        || check("STRIDER")
+        || check("TASKCLUSTER_ROOT_URL")
+        || check("JENKINS_URL")
+        || check("bamboo.buildKey")
+        || check("GO_PIPELINE_NAME")
+        || check("HUDSON_URL")
+        || check("WERCKER")
+        || check("MAGNUM")
+        || check("NEVERCODE")
+        || check("RENDER")
+        || check("SAIL_CI")
+        || check("SHIPPABLE")
+}
 
+fn check(name: &str) -> bool {
+    std::env::var(name).is_ok()
+}
